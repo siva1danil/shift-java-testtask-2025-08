daba0d0b481b94f611beb2c06622ccdd5bbb78ad
siva1danil <siva1danil@ya.ru>
2025-08-31 00:20:14 +0700

    Specify Main-Class for jar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,3 +41,9 @@ tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
 }
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "datasorter.App"
+    }
+}
